5a9bece924d0dc91704f95bb9b1737426395ab8e
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-11 23:08:02 +0800

    dev: Implement repositories

---
+++ src/main/java/com/charliechiang/wastesortinghelperserver/UserRepository.java
@@ -0,0 +1,9 @@
+package com.charliechiang.wastesortinghelperserver;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findById(long id);
+}
---
+++ src/main/java/com/charliechiang/wastesortinghelperserver/WasteRepository.java
@@ -0,0 +1,13 @@
+package com.charliechiang.wastesortinghelperserver;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface WasteRepository extends CrudRepository<Waste, Long> {
+    ArrayList<Waste> findByUser(String user);
+    ArrayList<Waste> findByCategory(WasteCategory category);
+    Waste findById(long id);
+}
