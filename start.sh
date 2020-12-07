e7e4e96e2e9ed2c22408053002c6c84bd0fd3096
Charlie Chiang <charlie_c_0129@outlook.com>
2020-12-07 10:12:48 +0800

    chore: fix a problem where start.sh may not start the application

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 JARPATH=$(ls target | grep ".jar$")
 JARPATH="target/"$JARPATH
-java -jar $(JARPATH) & echo $! > ./pid.file &
+# echo $JARPATH
+java -jar $JARPATH & echo $! > ./pid.file &
