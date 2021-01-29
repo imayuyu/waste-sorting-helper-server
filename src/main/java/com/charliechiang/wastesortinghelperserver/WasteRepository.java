ccc12552ebe0c7e1a620dcd3343880174d1d84b3
Charlie Chiang <charlie_c_0129@outlook.com>
2021-01-29 22:15:17 +0800

    feat: add a query method

--- src/main/java/com/charliechiang/wastesortinghelperserver/WasteRepository.java
+++ src/main/java/com/charliechiang/wastesortinghelperserver/WasteRepository.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 @Repository
 public interface WasteRepository extends JpaRepository<Waste, Long> {
     ArrayList<Waste> findTop5ByDustbinOrderByIdDesc(Dustbin dustbin);
+    ArrayList<Waste> findByDustbinOrderByIdDesc(Dustbin dustbin);
     ArrayList<Waste> findByUserOrderByIdDesc(User user);
     ArrayList<Waste> findTop20ByUserOrderByIdDesc(User user);
     ArrayList<Waste> findByCategory(WasteCategory category);
