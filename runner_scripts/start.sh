2ae915056f1bb9199d879970fa69424c3ec8a99f
Charlie Chiang <charlie_c_0129@outlook.com>
2021-07-25 01:55:21 +0800

    build: correct build script

--- runner_scripts/start.sh
+++ runner_scripts/start.sh
@@ -1,5 +1,1 @@
-docker run \
-    -p 10883:10883 \
-    -p 3306:3306 \
-    --add-host host.docker.internal:host-gateway \
-    charlie0129/waste-sorting-helper-server
+docker run -p 10883:10883 -p 3306:3306 --add-host host.docker.internal:host-gateway charlie0129/waste-sorting-helper-server
